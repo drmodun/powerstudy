463600829c97d924258ba1ceb9f58bde8f29ddbc
karloCroDev <karlo.webdev@gmail.com>
2024-09-29 13:08:07 +0200

    User chaning profile

--- app/(drawer)/user.tsx
+++ app/(drawer)/user.tsx
@@ -4,7 +4,6 @@ import {
     StyleSheet,
     SafeAreaView,
     View,
-    TouchableHighlight,
     TouchableOpacity,
 } from 'react-native';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
@@ -18,16 +17,56 @@ import { INDIGO, ROSE } from '@/constants/Colors';
 import { CustomText } from '@/components/CustomText';
 import { MyInput } from '@/components/Input';
 
-export default function user() {
+export default function User() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [image, setImage] = useState<string | null>(null);
 
     const condition = image || password || username;
 
+    // Unified function for API calls
+    const updateProfile = async (type: 'username' | 'password') => {
+        const data: {
+            name?: string;
+            password?: string;
+        } = {};
+        if (type === 'username' && username.length > 2) {
+            data.name = username;
+        } else if (type === 'password' && password.length > 7) {
+            data.password = password;
+        } else {
+            return;
+        }
+
+        try {
+            await fetch(`http://192.168.1.206:5500/users/{id}`, {
+                method: 'PATCH',
+                headers: {
+                    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzMsImVtYWlsIjoidGVzdEBnbWFpbC5jb20iLCJpYXQiOjE3Mjc2MDI1NzIsImV4cCI6MTcyNzY4ODk3Mn0.NAk-dnbMNaY9BzYBIllge6lYSt-vA5xs7wL7tkAE9Ys
+`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+            Toast.show({
+                type: 'success',
+                text1: 'Success',
+                text2: `You have successfully changed your ${type}`,
+            });
+            setUsername('');
+            setPassword('');
+        } catch (error) {
+            console.error(error);
+            Toast.show({
+                type: 'error',
+                text1: 'Error',
+                text2: `An error occurred while updating your ${type}. Please try again.`,
+            });
+        }
+    };
+
     const pickImage = async () => {
-        // No permissions request is necessary for launching the image library
-        let result = await ImagePicker.launchImageLibraryAsync({
+        const result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
             allowsEditing: true,
             aspect: [1, 1],
@@ -41,8 +80,10 @@ export default function user() {
 
     const uploadImage = async () => {
         if (!image) return;
+
         const form = new FormData();
         form.append('files', image);
+
         try {
             await fetch('http://192.168.1.206:5500/blob/images', {
                 method: 'POST',
@@ -55,53 +96,20 @@ export default function user() {
             Toast.show({
                 type: 'success',
                 text1: 'Success',
-                text2: 'You have successfuly changed an image',
+                text2: 'Image successfully updated!',
             });
+            setImage(null);
             router.push('/(drawer)/(tabs)/');
         } catch (error) {
             console.error(error);
             Toast.show({
                 type: 'error',
                 text1: 'Error',
-                text2: 'An error occurred while sending an image. Please try again in a few moments.',
+                text2: 'Error uploading image. Please try again.',
             });
-        } finally {
-            setImage(null);
         }
     };
 
-    // Change id
-    const updateCredentials = async () => {
-        if (!username || !password) return;
-        try {
-            await fetch('http://192.168.1.206:5500/users/{id}', {
-                method: 'PATCH',
-                headers: {
-                    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzMsImVtYWlsIjoidGVzdEBnbWFpbC5jb20iLCJpYXQiOjE3Mjc2MDI1NzIsImV4cCI6MTcyNzY4ODk3Mn0.NAk-dnbMNaY9BzYBIllge6lYSt-vA5xs7wL7tkAE9Ys`,
-                    'Content-Type': 'multipart/form-data',
-                },
-                body: JSON.stringify({
-                    name: username,
-                    password: password,
-                }),
-            });
-            Toast.show({
-                type: 'success',
-                text1: 'Success',
-                text2: 'You have successfuly changed your credentials',
-            });
-        } catch (error) {
-            console.error(error);
-            Toast.show({
-                type: 'error',
-                text1: 'Error',
-                text2: 'An error occurred while updating credentials. Please try again in a few moments.',
-            });
-        } finally {
-            setUsername('');
-            setPassword('');
-        }
-    };
     return (
         <SafeAreaView style={styles.wrapper}>
             <View style={styles.cardWrapper}>
@@ -112,24 +120,20 @@ export default function user() {
                                 image ??
                                 'https://randomuser.me/api/portraits/women/26.jpg',
                         }}
-                        width={120}
-                        height={120}
                         style={styles.userImg}
                     />
-                    <TouchableHighlight
-                        underlayColor="#fff"
+                    <TouchableOpacity
                         style={styles.editWrapper}
-                        activeOpacity={0.9}
                         onPress={pickImage}
                     >
                         <View style={styles.edit}>
                             <MaterialIcons
                                 name="edit"
                                 size={24}
-                                color={'white'}
+                                color="white"
                             />
                         </View>
-                    </TouchableHighlight>
+                    </TouchableOpacity>
                 </View>
                 <View>
                     <CustomText style={styles.username}>
@@ -155,34 +159,27 @@ export default function user() {
                 </View>
             </View>
             <View style={styles.buttonWrapper}>
-                <TouchableHighlight
-                    style={styles.button}
-                    underlayColor="#fff"
-                    onPress={() => {}}
-                    activeOpacity={0.9}
+                <TouchableOpacity
+                    disabled={!condition}
+                    onPress={() => {
+                        if (image) uploadImage();
+                        if (username) updateProfile('username');
+                        if (password) updateProfile('password');
+                    }}
+                    style={[
+                        styles.button,
+                        { backgroundColor: condition ? ROSE : '#767676' },
+                    ]}
                 >
-                    <TouchableOpacity
-                        disabled={!Boolean(condition)}
-                        onPress={uploadImage}
+                    <CustomText
                         style={[
-                            styles.buttonColor,
-                            {
-                                backgroundColor: condition ? ROSE : '#767676',
-                            },
+                            styles.buttonText,
+                            { color: condition ? 'white' : '#c0c0c0' },
                         ]}
                     >
-                        <CustomText
-                            style={[
-                                styles.buttonText,
-                                {
-                                    color: condition ? 'white' : '#c0c0c0',
-                                },
-                            ]}
-                        >
-                            Save changes
-                        </CustomText>
-                    </TouchableOpacity>
-                </TouchableHighlight>
+                        Save changes
+                    </CustomText>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     );
@@ -192,18 +189,15 @@ const styles = StyleSheet.create({
     wrapper: {
         paddingHorizontal: 8,
         paddingTop: 16,
-        display: 'flex',
-        flexDirection: 'column',
+        flex: 1,
         justifyContent: 'space-between',
-        height: '100%',
     },
     cardWrapper: {
-        display: 'flex',
-        gap: 16,
-        width: '100%',
         alignItems: 'center',
     },
     userImg: {
+        width: 120,
+        height: 120,
         borderRadius: 999,
     },
     userImgWrapper: {
@@ -224,39 +218,31 @@ const styles = StyleSheet.create({
         width: 40,
         borderRadius: 8,
         backgroundColor: INDIGO,
-        display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
     },
     editWrapper: {
         position: 'absolute',
-        top: -4,
-        left: 92,
+        top: 0,
+        right: -16,
         borderRadius: 8,
     },
-    buttonWrapper: { paddingVertical: 8 },
+    buttonWrapper: {
+        paddingVertical: 8,
+    },
     button: {
         borderRadius: 8,
         height: 64,
-        backgroundColor: ROSE,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     buttonText: {
         fontFamily: 'Outfit_500Medium',
-        color: 'white',
-    },
-    buttonColor: {
-        height: '100%',
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 8,
+        fontSize: 16,
     },
     inputWrapper: {
         width: '100%',
         paddingHorizontal: 16,
-        display: 'flex',
-        flexDirection: 'column',
         gap: 16,
         paddingVertical: 16,
     },
